84d4014e423fdf0200ab44e3d764fb0e02534734
Niklas Tran <niklastran2010@yahoo.de>
2026-08-23 12:32:46 +0200

    NPC looking at player

--- Assets/Scripts/Interaction System/NPC.cs
+++ Assets/Scripts/Interaction System/NPC.cs
@@ -8,22 +8,32 @@ public class NPC : DialogueObject
     [SerializeField] private int turnOffset = 25;
     private Quaternion initialRotation;
     [SerializeField] private float turnDuration = 0.5f;
+
+    private Animator animator;
+    [SerializeField] private float currentLookWeight = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+
     protected override void OnInteract()
     {
         base.OnInteract();
-        initialRotation = transform.rotation;
-        Vector3 direction = PersistentPlayer.Instance.transform.position - transform.position;
-        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        DOTween.To(() => currentLookWeight, x => currentLookWeight = x, 1f, turnDuration);
+    }
 
-        // Um 25 Grad auf der Y-Achse drehen (npc schauen etwas schief)
-        targetRotation *= Quaternion.Euler(0f, turnOffset, 0f);
-
-        transform.DORotateQuaternion(targetRotation, turnDuration);
+    private void OnAnimatorIK(int layerIndex)
+    {
+        Animator animator = GetComponent<Animator>();
+        animator.SetLookAtWeight(currentLookWeight, 0.3f, 0.8f, 1f);
+        animator.SetLookAtPosition(Camera.main.transform.position);
     }
 
     protected override void OnDialogueEnd(Dialogue _dialogue, Story _story)
     {
         base.OnDialogueEnd(_dialogue, _story);
-        transform.DORotateQuaternion(initialRotation, turnDuration);
+        DOTween.To(() => currentLookWeight, x => currentLookWeight = x, 0f, turnDuration);
     }
 }
